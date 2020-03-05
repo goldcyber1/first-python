b4bdb9f714b66cdd63dce97c1dc46848fc29e9df
goldcyber1 <goldcyber1@naver.com>
2020-03-05 15:09:22 +0900

    good commit 2

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model): # 모델을 정의하는 코드
+class Post(models.Model): 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
